b3af28f2a8e2fc471ce93563eaf49a8c4f0d2c3c
ahmedamine1990 <ahmedaminechaari@gmail.com>
2018-10-09 04:13:30 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import AppBar from 'material-ui/AppBar';
+
+const Index = () => {
+  return <div>
+    hello
+      </div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
